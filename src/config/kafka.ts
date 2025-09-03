87b0e25c680e7f3512f6979e7143202708b46b32
AhmedAfzal2089 <ameafzal@gmail.com>
2025-09-03 15:57:52 +0500

    add kafka consumer

---
+++ src/config/kafka.ts
@@ -0,0 +1,40 @@
+import { Consumer, EachMessagePayload, Kafka } from "kafkajs";
+import { MessageBroker } from "../types/broker";
+
+export class KafkaBroker implements MessageBroker {
+  private consumer: Consumer;
+  constructor(clientId: string, brokers: string[]) {
+    const kafka = new Kafka({ clientId, brokers });
+    this.consumer = kafka.consumer({ groupId: clientId });
+  }
+  /**
+   * Connect the consumer
+   */
+  async connectConsumer() {
+    await this.consumer.connect();
+  }
+
+  /**
+   * Disconnect the consumer
+   */
+  async disconnectConsumer() {
+    await this.consumer.disconnect();
+  }
+  async consumeMessage(topics: string[], fromBeginning: boolean = false) {
+    await this.consumer.subscribe({ topics, fromBeginning });
+    await this.consumer.run({
+      eachMessage: async ({
+        topic,
+        partition,
+        message,
+      }: EachMessagePayload) => {
+        // logic to handle incoming messages
+        console.log({
+          value: message.value.toString(),
+          topic,
+          partition,
+        });
+      },
+    });
+  }
+}
